678e0dbe6634ede5c2027d0605dc3c7f6b750c5f
Linnea Blad <bladlinnea@gmail.com>
2020-04-16 16:21:21 +0100

    Created class Dice for game Dice100

---
+++ src/Dice100/Dice.php
@@ -0,0 +1,63 @@
+<?php
+namespace liba19\Dice100;
+
+/**
+ * A dice with a number of different sides
+ */
+class Dice
+{
+    /**
+     * @var int  $dice  A dice with a number of sides.
+     * @var int $lastRoll  Value of the last roll.
+     */
+    private $dice;
+    private $lastRoll;
+
+
+
+    /**
+     * Constructor to initiate a dice.
+     *
+     * @param int $sides Number of sides the dice will have
+     */
+    public function __construct(int $sides = 6)
+    {
+        $this->dice  = $sides;
+    }
+
+
+
+    /**
+     * Roll the dice.
+     *
+     * @return int as the result of the dice roll
+     */
+    public function roll() : int
+    {
+        return $this->lastRoll = rand(1, $this->dice);
+    }
+
+
+
+    /**
+     * Return the last roll.
+     *
+     * @return int as the result of the last dice roll
+     */
+    public function getLastRoll() : int
+    {
+        return $this->lastRoll;
+    }
+
+
+
+    /**
+     * Return the amount of sides of the dice.
+     *
+     * @return int as the amount of sides
+     */
+    public function getSides() : int
+    {
+        return $this->dice;
+    }
+}
