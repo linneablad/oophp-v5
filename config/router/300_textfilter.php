f114f5f0545dcd24cdd451f3ca25ba3076014446
Linnea Blad <bladlinnea@gmail.com>
2020-05-04 08:28:22 +0100

    Added route for assignment Textfilter

---
+++ config/router/300_textfilter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * TextFilter Controller.
+ */
+return [
+    // Path where to mount the routes
+    "routes" => [
+        [
+            "info" => "TextFilter.",
+            "mount" => "textfilter",
+            "handler" => "\liba19\MyTextFilter\MyTextFilterController",
+        ],
+
+    ]
+];
